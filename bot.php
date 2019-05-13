67341222a6434080c1fe015f5100c801cafe20f5
bowon101 <50526642+bowon101@users.noreply.github.com>
2019-05-13 15:09:46 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -8,13 +8,13 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events']) {
+if (!is_null($events['ESP'])) {
 	
-	send_LINE($events['events']);
+	send_LINE($events['ESP']);
 		
 	echo "OK";
 	}
-/*if (!is_null($events['events'])) {
+if (!is_null($events['events'])) {
 	echo "line bot";
 	// Loop through each event
 	foreach ($events['events'] as $event) {
@@ -24,7 +24,34 @@ if (!is_null($events['events']) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
 
+  $messages = [
+        'type' => 'text',
+        'text' => $msg
+        //'text' => $text
+      ];
+
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+
+        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+
+      echo $result . "\r\n"; 
 			// Build message to reply back
 
 			$Topic = "NodeMCU1" ;
@@ -33,7 +60,7 @@ if (!is_null($events['events']) {
 			
 		}
 	}
-}*/
+}
 $Topic = "NodeMCU1" ;
 $text = "Test";
 getMqttfromlineMsg($Topic,$text);
