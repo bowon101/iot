3d9f58639c8a0ba39a5a1afdbd4097baf2adf3a7
bowon101 <50526642+bowon101@users.noreply.github.com>
2019-05-13 15:04:54 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -8,13 +8,13 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
+if (!is_null($events['events']) {
 	
-	send_LINE($events['ESP']);
+	send_LINE($events['events']);
 		
 	echo "OK";
 	}
-if (!is_null($events['events'])) {
+/*if (!is_null($events['events'])) {
 	echo "line bot";
 	// Loop through each event
 	foreach ($events['events'] as $event) {
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 			
 		}
 	}
-}
+}*/
 $Topic = "NodeMCU1" ;
 $text = "Test";
 getMqttfromlineMsg($Topic,$text);
