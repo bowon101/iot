65b95d15dbe072416aa0c8e3d8bd4d2b238241a1
bowon101 <50526642+bowon101@users.noreply.github.com>
2019-05-16 21:05:58 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,21 +1,10 @@
 <?php
-require("vendor/autoload.php");
- 
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
- 
-require("phpMQTT.php");
- 
-$mqtt = new phpMQTT("postman.cloudmqtt.com", 1883, "phpMQTT Pub Example"); //เปลี่ยน www.yourmqttserver.com ไปที่ mqtt server ที่เราสมัครไว้นะครับ
- 
-$token = "FBzQZx8xlOAa+Y1Wug39X9eS5CeGrncZKaLXCejdlIMjjBdrxTivKW4z0gxqYKc4AJ0TbafGH3HblKsIQ0G/+esWfmj5dJVqyARzvvbx1aASiRQaxSD4jcyjs2vgO7MRUJ6Sx/9jCFQig/nc3omTwgdB04t89/1O/w1cDnyilFU="; //นำ token ที่มาจาก line developer account ของเรามาใส่ครับ
- 
-$httpClient = new CurlHTTPClient($token);
-$bot = new LINEBot($httpClient, ['2f77160200bbb55edbe98b767f0ed52e' =&gt; $token]);
-// webhook
-$jsonStr = file_get_contents('php://input');
-$jsonObj = json_decode($jsonStr);
-print_r($jsonStr);
+//created by Pol.Cpl khomsan khemthong
+$access_token ='FBzQZx8xlOAa+Y1Wug39X9eS5CeGrncZKaLXCejdlIMjjBdrxTivKW4z0gxqYKc4AJ0TbafGH3HblKsIQ0G/+esWfmj5dJVqyARzvvbx1aASiRQaxSD4jcyjs2vgO7MRUJ6Sx/9jCFQig/nc3omTwgdB04t89/1O/w1cDnyilFU=';
+
+
+// Get POST body content
+$content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
